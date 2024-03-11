85236611c5f232eb53a244ba179f2795e05ed5c7
Jakub Malczak <118765990+Punio03@users.noreply.github.com>
2024-03-12 00:38:13 +0100

    Add files via upload

---
+++ Advanced techniques in C++ and STL/list1/zadanie2.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Jakub Malczak on 12/10/2023.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << R"(Instytut Informatyki Uniwersytetu Wrocławskiego
+ul. Joliot-Curie 15
+50-383 Wrocław
+C:\Program Files)" << endl;
+    cout << R"seq()")")")")')')seq";
+}
