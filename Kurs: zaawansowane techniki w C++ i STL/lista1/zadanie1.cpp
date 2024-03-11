750591c99ff7622b175be5bd4e93628efd746e64
Jakub Malczak <118765990+Punio03@users.noreply.github.com>
2024-03-12 00:46:31 +0100

    Add files via upload

---
+++ Kurs: zaawansowane techniki w C++ i STL/lista1/zadanie1.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Jakub Malczak on 12/10/2023.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "??= ??/ ??' ??( ??) ??! ??< ??> ??-" << endl;
+    cout << "Aby kompilator właściwie zinterpretował występujące "
+            "w programie trójznaki należy dodać -trigraphs przy kompilacji.";
+}
